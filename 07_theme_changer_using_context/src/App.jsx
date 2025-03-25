c60b470b3150e851dd69f5295a597f2b058fd0a0
Dina-Shanjida <dinasanjida0210@gmail.com>
2025-03-26 05:33:18 +0600

    just learning the functionalities

--- 07_theme_changer_using_context/src/App.jsx
+++ 07_theme_changer_using_context/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
 
+import { useEffect, useState } from 'react'
 import './App.css'
-import User_context_provider from './Context/User_Context_provider'
+import { ThemeProvider } from './Contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <User_context_provider>
-      <h1>Theme changer using Context API</h1>
-    </User_context_provider>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
