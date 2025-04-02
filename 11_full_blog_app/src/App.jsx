c0029e511a3d99e476fc36d304e921d8bb6b9865
Dina-Shanjida <dinasanjida0210@gmail.com>
2025-04-03 05:10:52 +0600

    RTK and components

--- 11_full_blog_app/src/App.jsx
+++ 11_full_blog_app/src/App.jsx
@@ -1,15 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
+import { useEffect } from 'react'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  
-  return (
+
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => { 
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  } , [])
+
+  return !loading ?(
     <>
-      <h1>agfuj</h1>      
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
     </>
-  )
+  ) : null
+
 }
 
 export default App
